574c6ac1ae1be7628279ec568f469a75ce38a8cb
Martin Paulsen <43757366+georgepaulsen@users.noreply.github.com>
2022-12-13 11:32:29 -0500

    Update CurrentBoard.jsx

--- src/Components/CurrentBoard.jsx
+++ src/Components/CurrentBoard.jsx
@@ -6,7 +6,7 @@ export const CurrentBoard = () => {
     <div>
       <iframe
         title="OIC Vestaboard"
-        src="https://simulator.vestaboard.com/?boardId=f8229c1b-bf79-4818-bb2c-25700f5251b0"
+        src`"https://simulator.vestaboard.com/?boardId=${board_id}`
         width="680"
         height="379.7"
         scrolling="no"
